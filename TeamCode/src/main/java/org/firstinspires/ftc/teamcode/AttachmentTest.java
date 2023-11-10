09e46f485922ef2a31b3aff52b6aefad35006cc9
FirstTechChallenge <FirstTechChallenge10303@gmail.com>
2023-11-09 18:41:48 -0800

    Roadrunner tuned. CV not working.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AttachmentTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AttachmentTest.java
@@ -1,11 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.attachments.ArmControl;
 import org.firstinspires.ftc.teamcode.attachments.ClawControl;
 import org.firstinspires.ftc.teamcode.attachments.LauncherControl;
 
+@Disabled
 @TeleOp(name="AttachmentTest", group = "Testing")
 public class AttachmentTest extends OpMode {
 
