5be95cab367c4046361bfcbfbf00766fd64e8e09
FirstTechChallenge <FirstTechChallenge10303@gmail.com>
2023-11-20 14:41:29 -0800

    All pixels for redright autonomous functioning and separated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedRightMiddlePixel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedRightMiddlePixel.java
@@ -14,11 +14,14 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 public class RedRightMiddlePixel extends LinearOpMode {
     private ArmControl Arm = null;
     private ClawControl Claw = null;
-    private int spikeMark = 2;
+    private int spikeMark = 3;
 
     //trajectory fields
     private SampleMecanumDrive drive;
+    private Trajectory rTraj1, rTraj2, rTraj3, rTraj4, rTraj5, rTraj6 = null;
     private Trajectory mTraj1, mTraj2, mTraj3, mTraj4, mTraj5, mTraj6 = null;
+    private Trajectory lTraj1, lTraj1prime, lTraj2, lTraj3, lTraj4, lTraj5, lTraj6 = null;
+
 
     public void runOpMode() throws InterruptedException {
 
@@ -31,9 +34,15 @@ public class RedRightMiddlePixel extends LinearOpMode {
 
         //trajectories are flipped direction
 
-        if(spikeMark == 2) {
+        if(spikeMark == 1) {
+            //right pixel trajectories
+            buildRightPixelTraj();
+        } else if(spikeMark == 2) {
             //middle pixel trajectories
             buildMiddlePixelTraj();
+        } else if(spikeMark == 3) {
+            //left pixel trajectories
+            buildLeftPixelTraj();
         }
 
         //start
@@ -45,11 +54,64 @@ public class RedRightMiddlePixel extends LinearOpMode {
         Claw.close();
         sleep(1000);
 
-        if(spikeMark == 2) {
+        if(spikeMark == 1) {
+            followRightPixelTraj();
+        } else if(spikeMark == 2) {
             followMiddlePixelTraj();
+        } else if(spikeMark == 3) {
+            followLeftPixelTraj();
         }
     }
 
+
+    //build right pixel trajectories
+    private void buildRightPixelTraj() {
+        rTraj1 = drive.trajectoryBuilder(new Pose2d())
+                .lineToSplineHeading(new Pose2d(-26.5, 3, Math.toRadians(-25)))
+                .build();
+        rTraj2 = drive.trajectoryBuilder(rTraj1.end())
+                .lineToSplineHeading(new Pose2d(-15, 0, Math.toRadians(-90)))
+                .build();
+        rTraj3 = drive.trajectoryBuilder(rTraj2.end())
+                .back(29)
+                .build();
+        rTraj4 = drive.trajectoryBuilder(rTraj3.end())
+                .strafeRight(7)
+                .build();
+        rTraj5 = drive.trajectoryBuilder(rTraj4.end())
+                .strafeLeft(18.5)
+                .build();
+        rTraj6 = drive.trajectoryBuilder(rTraj5.end())
+                .back(17)
+                .build();
+    }
+
+    //follow right pixel trajectories
+    private void followRightPixelTraj() {
+        //deliver purple pixel
+        drive.followTrajectory(rTraj1);
+        drive.followTrajectory(rTraj2);
+        drive.followTrajectory(rTraj3);
+        drive.followTrajectory(rTraj4);
+
+        //deliver the pixel
+        Arm.autoDeliver();
+        Claw.autoDeliver();
+        waitForArm();
+
+        //open the arm
+        Claw.open();
+        sleep(1000);
+
+        //retract
+        Arm.reset();
+        Claw.reset();
+        waitForArm();
+
+        //park
+        drive.followTrajectory(rTraj5);
+        drive.followTrajectory(rTraj6);
+    }
 
     //build middle pixel trajectories
     private void buildMiddlePixelTraj() {
@@ -100,6 +162,59 @@ public class RedRightMiddlePixel extends LinearOpMode {
         //park
         drive.followTrajectory(mTraj5);
         drive.followTrajectory(mTraj6);
+    }
+
+    //build left pixel trajectories
+    private void buildLeftPixelTraj() {
+        lTraj1 = drive.trajectoryBuilder(new Pose2d())
+                .lineToSplineHeading(new Pose2d(-16.5, 0, Math.toRadians(45)))
+                .build();
+        lTraj1prime = drive.trajectoryBuilder(lTraj1.end())
+                .back(11.5)
+                .build();
+        lTraj2 = drive.trajectoryBuilder(lTraj1prime.end())
+                .lineToSplineHeading(new Pose2d(-15, 0, Math.toRadians(-90)))
+                .build();
+        lTraj3 = drive.trajectoryBuilder(lTraj2.end())
+                .back(29)
+                .build();
+        lTraj4 = drive.trajectoryBuilder(lTraj3.end())
+                .strafeRight(22)
+                .build();
+        lTraj5 = drive.trajectoryBuilder(lTraj4.end())
+                .strafeLeft(33)
+                .build();
+        lTraj6 = drive.trajectoryBuilder(lTraj5.end())
+                .back(16)
+                .build();
+    }
+
+    //follow left pixel trajectories
+    private void followLeftPixelTraj() {
+        //deliver purple pixel
+        drive.followTrajectory(lTraj1);
+        drive.followTrajectory(lTraj1prime);
+        drive.followTrajectory(lTraj2);
+        drive.followTrajectory(lTraj3);
+        drive.followTrajectory(lTraj4);
+
+        //deliver the pixel
+        Arm.autoDeliver();
+        Claw.autoDeliver();
+        waitForArm();
+
+        //open the arm
+        Claw.open();
+        sleep(1000);
+
+        //retract
+        Arm.reset();
+        Claw.reset();
+        waitForArm();
+
+        //park
+        drive.followTrajectory(lTraj5);
+        drive.followTrajectory(lTraj6);
     }
 
     //wait for the arm and claw to deliver
